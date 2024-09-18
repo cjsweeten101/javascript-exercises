6531c405810fecf890f188c95dde6c37a731f52d
Clayton Sweeten <cjsweeten101@gmail.com>
2024-09-18 10:25:20 -0600

    Solution 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(argString, repeatNumber) {
+    if (repeatNumber < 0) {
+        return "ERROR";
+    } else {
+        let result = '';
+        for (let i = 0; i < repeatNumber; i++) {
+            result += argString;
+        }    
+    return result;
+    }
 };
 
 // Do not edit below this line
