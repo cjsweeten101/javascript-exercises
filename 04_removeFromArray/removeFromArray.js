55323516b3b41d6a35f0f05ba45415446709e57c
Clayton Sweeten <cjsweeten101@gmail.com>
2024-09-18 11:44:01 -0600

    Solution 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray) {
+    let result = inputArray;
+    let valuesToBeRemoved = Array.from(arguments);
+    valuesToBeRemoved.shift();
+    for (const value of result) {
+        for (const argVal of valuesToBeRemoved) {
+            if (value === argVal) {
+                result = result.toSpliced(result.indexOf(value), 1)
+            }
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
