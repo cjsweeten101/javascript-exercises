600c000eeaab02fa5e9aa0c096ee41adcba49488
Clayton Sweeten <cjsweeten101@gmail.com>
2024-09-27 12:02:07 -0600

    solution 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,34 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
+};
+
+const subtract = function(a, b) {
+  return a - b;
+};
+
+const sum = function(array) {
+  return array.reduce((accum, current) => accum + current, 0);
+};
+
+const multiply = function(array) {
+  return array.reduce((accumulator, current) => accumulator * current);
+};
+
+const power = function(a, n) {
+  return a**n;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(a) {
+  let result = 1;
+  if ( a === 0 ) {
+    return result;
+  } else {
+    for (let i = 1; i <= a; i++ ) {
+      result *= i;
+    }
+    return result;
+  }
 	
 };
 
