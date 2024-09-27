e248698e3d7b8a0ca1773751f4e457c1d31fc117
Clayton Sweeten <cjsweeten101@gmail.com>
2024-09-27 12:48:11 -0600

    solution 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let arr = string.toLowerCase()
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,'')
+    .replace(/\s/g, '')
+    .split('');
+    let reverse = arr.slice().reverse();
+    return arr.every((val, index) => reverse[index] === arr[index]);
 };
+
+console.log(palindromes('A car, a man, a maraca.'));
 
 // Do not edit below this line
 module.exports = palindromes;
